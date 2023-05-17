224378ab8cb95c9811e05c578b69e0dc52dd1a34
Klaudia Ratkowska <103277814+KlaudiaRatkowska@users.noreply.github.com>
2023-05-17 11:38:51 +0200

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lab1MPWI
+{
+    public class Permutations
+    {
+       static int numberOfPerm = 0;
+        static void permRec(String nb, char[] arr, int last, int idx)
+        {
+            int len = nb.Length;
+            for (int i = 0; i < len; i++)
+            {
+                arr[idx] = nb[i];
+                if (idx == last)
+                {
+                    Console.WriteLine(new String(arr));
+                    numberOfPerm++;
+                }
+                else
+                    permRec(nb, arr, last, idx + 1);
+            }
+        }
+        static void permutations(String nb)
+        {
+            int len = nb.Length;
+            char[] temp = nb.ToCharArray();
+            Array.Sort(temp);
+            nb = new String(temp);
+            char[] arr = new char[len + 1];
+            permRec(nb, arr, len - 1, 0);
+
+        }
+        public static void Main(String[] args)
+        {
+            String number;
+            number = Console.ReadLine();
+            permutations(number);
+            Console.WriteLine(numberOfPerm);
+        }
+    }
+}
